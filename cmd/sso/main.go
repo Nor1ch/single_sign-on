0ce42df48c085d315c7b55dccc28e8ab56f69814
ilia_cherepanin <iliacherepanin@gmail.com>
2024-04-25 15:15:07 +0300

    added graceful shutdown added logic for grpc handlers

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+	"os/signal"
 	"sso/internal/app"
 	"sso/internal/config"
 	"sso/pkg/logger/slogpretty"
+	"syscall"
 )
 
 func main() {
@@ -14,5 +18,15 @@ func main() {
 	logger.Info("service starting")
 
 	application := app.New(logger, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
-	application.GRPCServer.MustRun()
+	go application.GRPCServer.MustRun()
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+	logger.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCServer.Stop()
+	logger.Info("application stopped")
 }
